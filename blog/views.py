153ff85ec94f338ba6753f5efa1dc50cfbb983ba
HanZawNyein <hanzawnyineonline@gmail.com>
2022-11-28 08:13:06 +0630

    canonical URL for SEO

--- blog/views.py
+++ blog/views.py
@@ -9,9 +9,9 @@ def post_list(request):
     return render(request, 'blog/post/list.html', {'posts': posts})
 
 
-def post_detail(request, id):
+def post_detail(request, year,month,day,post):
     # try:
-    post = get_object_or_404(Post, id=id, status=Post.Status.PUBLISHED)
+    post = get_object_or_404(Post,status=Post.Status.PUBLISHED,publish__year=year,publish__month=month,publish__day=day,slug=post)
     # Post.published.get(id=id)
     # except Post.DoesNotExist:
     #     raise Http404("No Posts Found.")
