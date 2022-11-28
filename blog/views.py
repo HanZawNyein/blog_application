1ba75a2cda42d2f9def6f8238cdb8ab4965e2283
HanZawNyein <hanzawnyineonline@gmail.com>
2022-11-28 09:02:55 +0630

    class based views

--- blog/views.py
+++ blog/views.py
@@ -1,20 +1,20 @@
 from django.shortcuts import render, get_object_or_404
 from .models import Post
-from django.http import Http404
-from django.core.paginator import Paginator,EmptyPage,PageNotAnInteger
+# from django.http import Http404
+# from django.core.paginator import Paginator,EmptyPage,PageNotAnInteger
 
-# Create your views here.
-def post_list(request):
-    post_list = Post.published.all()
-    paginator = Paginator(post_list,1)
-    page_number = request.GET.get('page',1)
-    try:
-        posts = paginator.page(page_number)
-    except PageNotAnInteger:
-        posts = paginator.page(1)
-    except EmptyPage:
-        posts = paginator.page(paginator.num_pages)
-    return render(request, 'blog/post/list.html', {'posts': posts})
+# # Create your views here.
+# def post_list(request):
+#     post_list = Post.published.all()
+#     paginator = Paginator(post_list,1)
+#     page_number = request.GET.get('page',1)
+#     try:
+#         posts = paginator.page(page_number)
+#     except PageNotAnInteger:
+#         posts = paginator.page(1)
+#     except EmptyPage:
+#         posts = paginator.page(paginator.num_pages)
+#     return render(request, 'blog/post/list.html', {'posts': posts})
 
 
 def post_detail(request, year,month,day,post):
@@ -24,3 +24,11 @@ def post_detail(request, year,month,day,post):
     # except Post.DoesNotExist:
     #     raise Http404("No Posts Found.")
     return render(request, 'blog/post/detail.html', {'post': post})
+
+from django.views.generic import ListView
+
+class PostListView(ListView):
+    queryset = Post.published.all()
+    context_object_name='posts'
+    paginate_by: int=1
+    template_name: str='blog/post/list.html'
