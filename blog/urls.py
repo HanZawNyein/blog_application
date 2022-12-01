c36fda7e32a5591dd7e83d10c5d5f405f4e4cc6a
HanZawNyein <hanzawnyineonline@gmail.com>
2022-12-01 21:16:02 +0630

    add rss feed

--- blog/urls.py
+++ blog/urls.py
@@ -1,5 +1,6 @@
 from . import views
 from django.urls import path
+from .feeds import LatestPostFeed
 
 app_name = 'blog'
 
@@ -10,4 +11,8 @@ urlpatterns = [
     path('<int:year>/<int:month>/<int:day>/<slug:post>/', views.post_detail, name='post_detail'),
     path('<int:post_id>/share/',views.post_share,name='post_share'),
     path('<int:post_id>/comment/',views.post_comment,name='post_comment'),
+
+    #feed
+    path('feed/',LatestPostFeed(),name='post_feed'),
+    # https://github.com/yang991178/fluent-reader/releases/tag/v1.1.3
 ]
